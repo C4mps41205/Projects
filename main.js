4d5180aab066b8a8a9bb71576a10e27f9665d278
C4mps41205 <lucasmmpacheco75@gmail.com>
2022-05-29 18:11:29 -0300

    git config --global user.email lucasmmpacheco75@gmail.com

---
+++ main.js
@@ -0,0 +1,16 @@
+var a;
+var b;
+var c;
+
+
+document.getElementById("botao").onclick = function(){
+    a = document.getElementById("aside").value;
+    a = Number(a);
+    b = document.getElementById("bside").value;
+    b = Number(b);
+
+    c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+	
+    document.getElementById("valor").innerHTML = "c side: " + c;
+}
+
